32e805349f1b661d2457c0fcd22775b229cfa375
Lito Jr Galan <galanlito.94@gmail.com>
2023-11-29 09:01:20 +0800

    build the app

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-export default {
-  base: '/web-portfolio-new/',
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
